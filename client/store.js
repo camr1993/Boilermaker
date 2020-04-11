8d328ac985561ce3ea6288536a82481e47bac19c
Cameron Ratliff <cratliff@colgate.edu>
2020-04-11 11:27:06 -0400

    redux fix Provider import

--- client/store.js
+++ client/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import { reducer } from './reducer';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import loggerMiddleware from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(thunkMiddleware, createLogger()))
+  composeWithDevTools(applyMiddleware(thunkMiddleware, loggerMiddleware))
 );
 
 export default store;
